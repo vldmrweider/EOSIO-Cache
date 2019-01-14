fb46a19c551685748e674ff8fb80be47b63a55fc
Vladimir <vldmr.weider@gmail.com>
2019-01-14 16:03:28 +0300

    prop rm

--- src/main/java/com/pugachev/emr/EmrApplication.java
+++ src/main/java/com/pugachev/emr/EmrApplication.java
@@ -2,17 +2,23 @@ package com.pugachev.emr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 //@ComponentScan({"com.pugachev.emr.service","com.pugachev.emr.repository"})
 @EnableMongoRepositories("com.pugachev.emr.repository")
-public class EmrApplication {
+public class EmrApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(EmrApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EmrApplication.class);
+    }
 }
 
