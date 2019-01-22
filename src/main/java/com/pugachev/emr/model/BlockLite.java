bfde469a39fab3b3e69abfbfb756dc140f71bfd5
Vladimir <vldmr.weider@gmail.com>
2019-01-22 11:25:39 +0300

    prop rm

---
+++ src/main/java/com/pugachev/emr/model/BlockLite.java
@@ -0,0 +1,25 @@
+package com.pugachev.emr.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "blocks")
+public class BlockLite {
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    private int block_num;
+
+    public int getBlock_num() {
+        return block_num;
+    }
+
+    public void setBlock_num(int block_num) {
+        this.block_num = block_num;
+    }
+
+}
